08650b2eb640d317fb3bcfc319519d0b4cf8847c
Shraddha Jadhav <32254132+shraddha1998@users.noreply.github.com>
2019-07-26 19:12:15 +0530

    Add files via upload

--- rectifiedLinearActivationFunction.py
+++ rectifiedLinearActivationFunction.py
@@ -3,25 +3,26 @@ The rectified linear activation function (called ReLU) leads to very high-perfor
 
 Here are some examples:
 relu(3) = 3
-relu(-3) = 0 '''
-
-import numpy as np
+relu(-3) = 0 
+'''
 def relu(input):
     '''Definition relu activation function is here'''
     output = max(input,0)
     return(output)
-
+import numpy as np
 input_data=np.array([2,1])
 
-weights={'node_0':np.array([2,4]), 'node_1':np.array([4,-5]),'output':np.array([2,7])
+weights={'node_0':np.array([-4,2]), 'node_1':np.array([1,5]),'output':np.array([2,1])
+}
 
-node_0_input = (input_data * weights['node_0']).sum()
-node_0_output = relu(node_0_input)
+node_0_value = (input_data * weights['node_0']).sum()
+node_0_output = relu(node_0_value)
 
-node_1_input = (input_data * weights['node_1']).sum()
-node_1_output = relu(node_1_input)
+nodeinn = (input_data * weights['node_1']).sum()
+nodeoutt = relu(nodeinn)
 
-hidden_layer_outputs = np.array([node_0_output, node_1_output])
+hidden_layer_outputs = np.array([node_0_output, nodeoutt])
 
 # model output relu function is not applied here 
 model_output = (hidden_layer_outputs * weights['output']).sum()
+print(model_output)
